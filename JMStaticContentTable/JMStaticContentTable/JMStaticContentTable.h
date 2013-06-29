6df79d469bc0e49f3b73ee6ffc24de7a9e25b1a5
gmarik <gmarik@gmail.com>
2013-06-29 12:54:30 -0500

    xcode boilerplate

---
+++ JMStaticContentTable/JMStaticContentTable/JMStaticContentTable.h
@@ -0,0 +1,13 @@
+//
+//  JMStaticContentTable.h
+//  JMStaticContentTable
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Jake Marsh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMStaticContentTable : NSObject
+
+@end
---
+++ JMStaticContentTable/JMStaticContentTableTests/JMStaticContentTableTests.m
@@ -0,0 +1,36 @@
+//
+//  JMStaticContentTableTests.m
+//  JMStaticContentTableTests
+//
+//  Created by Maryan Hratson on 2013-06-29.
+//  Copyright (c) 2013 Jake Marsh. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JMStaticContentTableTests : XCTestCase
+
+@end
+
+@implementation JMStaticContentTableTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
